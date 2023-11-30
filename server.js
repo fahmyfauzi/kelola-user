15f0c03965a1272e9eb5b89111e206bbe4cd13d5
fahmyfauzi <fahmyfauzii@gmail.com>
2023-11-30 22:15:41 +0700

    setting up server

---
+++ server.js
@@ -0,0 +1,33 @@
+//import packages
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import colors from "colors";
+
+//setup dotenv
+dotenv.config();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+//routes
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+//port
+const port = process.env.PORT || 3000;
+
+//listen port server
+app.listen(port, () => {
+  console.log(
+    `Server is running in ${process.env.DEV_MODE} on http://localhost:${port}`
+      .bgBlue.white
+  );
+});
