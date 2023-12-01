90e991a917a3b1c3ced86670198b1f12d725a48a
Fahmy <fahmyfauzii@gmail.com>
2023-12-01 08:53:11 +0700

    Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,56 +1,100 @@
 //import packages
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 //import files
-import User from "../models/userModel.js";
+import User from '../models/userModel.js';
 
-const register = async (req, res, next) => {
-  //define
-  const { nama, email, password } = req.body;
+const register = async(req, res, next) => {
+    //define
+    const { nama, email, password } = req.body;
 
-  //validate name
-  if (!nama) {
-    next("Nama harus di isi");
-  }
+    //validate name
+    if (!nama) {
+        next('Nama harus di isi');
+    }
 
-  //validate email
-  if (!email) {
-    next("email harus di isi");
-  }
+    //validate email
+    if (!email) {
+        next('email harus di isi');
+    }
 
-  //validate password
-  if (!password) {
-    next("Password harus di isi");
-  }
+    //validate password
+    if (!password) {
+        next('Password harus di isi');
+    }
 
-  //validate email already
-  const userAvailable = await User.findOne({ email });
-  if (userAvailable) {
-    res.status(200).json({
-      success: false,
-      message: "Email already register please login",
+    //validate email already
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
+        res.status(200).json({
+            success: false,
+            message: 'Email already register please login',
+        });
+    }
+
+    //hashing password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    //create user
+    const user = await User.create({
+        nama,
+        email,
+        password: hashPassword,
     });
-  }
 
-  //hashing password
-  const hashPassword = await bcrypt.hash(password, 10);
-
-  //create user
-  const user = await User.create({
-    nama,
-    email,
-    password: hashPassword,
-  });
-
-  return res.status(201).json({
-    success: true,
-    message: "User created successfully",
-    data: {
-      nama: user.nama,
-      email: user.email,
-      password: user.password,
-    },
-  });
+    return res.status(201).json({
+        success: true,
+        message: 'User created successfully',
+        data: {
+            nama: user.nama,
+            email: user.email,
+            password: user.password,
+        },
+    });
 };
 
-export { register };
+const login = async(req, res, next) => {
+    //define
+    const { email, password } = req.body;
+
+    //validate email
+    if (!email) {
+        next('email harus di isi');
+    }
+
+    //validate password
+    if (!password) {
+        next('password harus di isi');
+    }
+
+    //cek email
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        //setup jwt
+        const accessToken = jwt.sign({
+                //payload data
+                user: {
+                    id: user.id,
+                    email: user.email,
+                    nama: user.nama,
+                },
+            },
+            process.env.SECRET_KEY, { expiresIn: '1d' }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: 'Login success',
+            user,
+            accessToken,
+        });
+    } else {
+        return res.status(401).json({
+            success: false,
+            message: 'Email atau password salah!',
+        });
+    }
+};
+
+export { register, login };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,15 @@
 //import packages
-import express from "express";
+import express from 'express';
 
 //import files
-import { register } from "../controllers/authController.js";
+import { register, login } from '../controllers/authController.js';
 
 //rest object
 const router = express.Router();
 
 //route
-router.post("/register", register);
+router.post('/register', register);
+
+router.post('/login', login);
 
 export default router;
