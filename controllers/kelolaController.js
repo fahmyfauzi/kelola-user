eee26d5cc328639e8edf06ab24bfd5a5234a6537
Fahmy <fahmyfauzii@gmail.com>
2023-12-14 13:34:53 +0700

    add admin

--- controllers/kelolaController.js
+++ controllers/kelolaController.js
@@ -1,6 +1,7 @@
 //import files
 import User from '../models/userModel.js';
 import Customer from '../models/customerModel.js';
+import Admin from '../models/adminModel.js';
 
 const getAllCustomer = async (req, res, next) => {
   const customer = await User.find({ role: 'customer' }).select('-password');
@@ -52,4 +53,55 @@ const addCustomerHandler = async (req, res, next) => {
     });
   }
 };
-export { getAllCustomer, getAllAdmin, addCustomerHandler };
+
+const getCustomersHandler = async (req, res, next) => {
+  const customer = await Customer.find();
+  res.status(200).json({
+    success: true,
+    count: customer.length,
+    data: customer,
+  });
+};
+
+const addAdminHandler = async (req, res, next) => {
+  try {
+    const { nama, alamat, noHp } = req.body;
+
+    if (!nama || !alamat || !noHp) {
+      return res.status(400).json({
+        success: false,
+        message: 'Nama, Alamat, dan No Hp harus diisi',
+      });
+    }
+
+    // create user
+    const admin = await Admin.create({
+      nama,
+      alamat,
+      noHp,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: 'Admin created successfully',
+      data: admin,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: 'Internal Server Error',
+    });
+  }
+};
+
+const getAdminHandler = async (req, res, next) => {
+  const admin = await Admin.find();
+  res.status(200).json({
+    success: true,
+    count: admin.length,
+    data: admin,
+  });
+};
+
+export { getAllCustomer, getAllAdmin, addCustomerHandler, getCustomersHandler, addAdminHandler, getAdminHandler };
--- routes/kelolaRoute.js
+++ routes/kelolaRoute.js
@@ -3,14 +3,19 @@ import express from 'express';
 
 //import files
 import { verifyToken, verifyTokenAndAdmin } from '../middlewares/authMiddleware.js';
-import { getAllAdmin, getAllCustomer, addCustomerHandler } from '../controllers/kelolaController.js';
+import { getAllAdmin, getAllCustomer, addCustomerHandler, getCustomersHandler, addAdminHandler, getAdminHandler } from '../controllers/kelolaController.js';
 
 const router = express.Router();
 
 //route bisa diakses oleh user biasa
 router.get('/customer', getAllCustomer);
-router.get('/admin', getAllAdmin);
+router.get('/admins', getAllAdmin);
 
-router.post('/customer', addCustomerHandler);
+//crud
+router.get('/customers', getCustomersHandler);
+router.post('/customers', addCustomerHandler);
+
+router.get('/admin', getAdminHandler);
+router.post('/admin', addAdminHandler);
 
 export default router;
